402e38561ec6b56e53f84119f0dc98943a5bfb21
Denis Romanovskiy <53230856+Skillpolice@users.noreply.github.com>
2020-12-20 20:16:01 +0300

    !

---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public Bullet bulletPrefab;
+    public GameObject shootPosBullet;
+
+    [Header("Bullet")]
+    public float fireRotate; //частота стрельбы
+    float nextFire; //сколько прошло времени от предыдущего выстрела
+
+
+    private void Awake()
+    {
+
+    }
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Fire1") && nextFire <= 0)
+        {
+            Instantiate(bulletPrefab, shootPosBullet.transform.position, transform.rotation); //Создание пули , префаб, откуда идем выстрел и нужное вращение
+            nextFire = fireRotate;
+        }
+        if(nextFire > 0)
+        {
+            nextFire -= Time.deltaTime;
+        }
+    }
+}
