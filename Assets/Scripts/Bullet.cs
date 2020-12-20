fe78773b1d69ae114e9a54e9841145cdf57c177e
Denis Romanovskiy <53230856+Skillpolice@users.noreply.github.com>
2020-12-20 19:49:40 +0300

    Initial commit

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float bullSpeed;
+
+    Rigidbody2D rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Start()
+    {
+        rb.velocity = -transform.up * bullSpeed; //Скорость пули - стреляет куда смотрит
+    }
+
+    private void Update()
+    {
+         
+    }
+}
---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    Rigidbody2D rb;
+
+    [Header("Speed Player")]
+    public float speed;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update() // Движение персонажа
+    {
+        Move();
+        Rotate();
+    }
+
+    private void Move()
+    {
+        float inputX = Input.GetAxis("Horizontal");
+        float inputY = Input.GetAxis("Vertical");
+
+        Vector3 direction = new Vector3(inputX, inputY);
+
+        if (direction.magnitude > 1) //если длинна вектора > 1, нормалезуем дистанцию
+        {
+            direction = direction.normalized;
+        }
+        rb.velocity = direction * speed;
+    }
+    public void Rotate()
+    {
+        Vector3 playerPos = transform.position;
+        Vector3 mouseWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        Vector3 direction = playerPos - mouseWorldPos;
+        direction.z = 0;
+        transform.up = direction;
+
+    }
+}
